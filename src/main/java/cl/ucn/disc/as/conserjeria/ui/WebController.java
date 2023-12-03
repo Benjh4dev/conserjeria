ff8c7599f60aeeb1e952d0b4e281ee3445fb042d
Benjh4 <benjamin.plaza@alumnos.ucn.cl>
2023-12-03 11:44:38 -0300

    added grpc endpoint

--- src/main/java/cl/ucn/disc/as/conserjeria/ui/WebController.java
+++ src/main/java/cl/ucn/disc/as/conserjeria/ui/WebController.java
@@ -1,11 +1,17 @@
 package cl.ucn.disc.as.conserjeria.ui;
 
 import cl.ucn.disc.as.conserjeria.exceptions.SistemaException;
+import cl.ucn.disc.as.conserjeria.grpc.PersonaGrpc;
+import cl.ucn.disc.as.conserjeria.grpc.PersonaGrpcRequest;
+import cl.ucn.disc.as.conserjeria.grpc.PersonaGrpcResponse;
+import cl.ucn.disc.as.conserjeria.grpc.PersonaGrpcServiceGrpc;
 import cl.ucn.disc.as.conserjeria.model.Persona;
 import cl.ucn.disc.as.conserjeria.services.Sistema;
 import cl.ucn.disc.as.conserjeria.services.SistemaImpl;
 
 import io.ebean.DB;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
 import io.javalin.Javalin;
 import io.javalin.http.NotFoundResponse;
 
@@ -53,6 +59,41 @@ public final class WebController implements RoutesConfigurator {
                 e.printStackTrace();
             }
             ctx.json(oPersona.orElseThrow(() -> new NotFoundResponse("Can't find persona with ruth: " + rut)));
+        });
+
+        app.get("/api/grpc/personas/rut/{rut}", ctx -> {
+
+            String rut = ctx.pathParam("rut");
+
+            // the channel
+            ManagedChannel channel = ManagedChannelBuilder
+                    .forAddress("127.0.0.1", 50123)
+                    .usePlaintext()
+                    .build();
+
+            // stub
+            PersonaGrpcServiceGrpc.PersonaGrpcServiceBlockingStub stub =
+                    PersonaGrpcServiceGrpc.newBlockingStub(channel);
+
+            // call the gprc
+            PersonaGrpcResponse response = stub.retrieve(PersonaGrpcRequest
+                    .newBuilder()
+                    .setRut("130144918")
+                    .build());
+            // get the response
+            PersonaGrpc personaGrpc = response.getPersona();
+
+
+            // return the persona
+            Optional<Persona> oPersona = Optional.of(Persona.builder()
+                    .rut(personaGrpc.getRut())
+                    .nombre(personaGrpc.getNombre())
+                    .apellidos(personaGrpc.getApellidos())
+                    .email(personaGrpc.getEmail())
+                    .build());
+
+            ctx.json(oPersona.orElseThrow(() -> new NotFoundResponse("Can't find Persona with rut: " + rut)));
+
         });
     }
 
